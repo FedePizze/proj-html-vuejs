8f61171c308be0ebe6f38fa980f0a1a76e7d5c02
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-03-03 12:41:41 +0100

    img

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div>
+    <MyHeader></MyHeader>
+  </div>
+
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/style/general.scss";
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="navBar">
+    <div class="box">
+        <img src="../assets/img/logo-sidearea-1.png" alt="">
+    </div>
+    
+    <div class="box">
+        <nav>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">PAGES</a></li>
+                <li><a href="">PORTFOLIO</a></li>
+                <li><a href="">BLOG</a></li>
+                <li><a href="">SHOP</a></li>
+                <li><a href="">ELEMENTS</a></li>
+                <li><a href=""><img id="sixDots" src="../assets/svg/svg-1.svg" alt=""></a></li>
+            </ul>
+        </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MyHeader"
+}
+</script>
+
+<style scoped lang="scss">
+    #navBar{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 95%;
+        margin: 20px auto;
+
+        img{
+            width: 80%;
+        }
+
+        ul{
+            display: flex;
+            align-items: center;
+            gap: 40px;
+
+            li{
+                list-style: none;
+
+                a{
+                  text-decoration: none;
+                  font-size: 11px;
+                  font-weight: 600;
+                  color: black;
+                  letter-spacing: 3px;
+                }
+            }
+        }
+
+        
+    }
+
+</style>
